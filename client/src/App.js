96e486e8a51d21bf8f509608416c3f891c12776c
symphony <jciurc@gmail.com>
2022-06-10 10:50:35 -0600

    add axios to client. make example get request to backend from react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import './styles/App.css';
 import 'normalize.css';
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    axios.get('/students')
+      .then((res) => {
+        console.log('heere', res);
+        setStudents(res.data);
+      });
+  }, []);
+
+
   return (
     <div className="App">
       "Hello World :D"
+      {students[0]?.first_name}
     </div>
   );
 }
