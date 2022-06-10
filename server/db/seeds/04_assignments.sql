6b0a90b19dea663235a77919a2b6bd13a276d523
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-10 14:58:48 -0400

    Add outline for CreateAssignment

--- server/db/seeds/04_assignments.sql
+++ server/db/seeds/04_assignments.sql
@@ -1,8 +1,8 @@
 -- seeds/03_assignments.sql
 -- Seed Rick
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.google.ca');
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.intergalacticebay.gl');
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.plumbus.gl');
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.fedaration.com');
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.torontopubliclibrary.ca');
-INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  2, 'my description', 'www.ontario.ca');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment1',  1,  1, 'my description', 'www.google.ca');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment2',  1,  1, 'my description', 'www.intergalacticebay.gl');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment3',  1,  1, 'my description', 'www.plumbus.gl');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment4',  1,  2, 'my description', 'www.fedaration.com');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment5',  1,  2, 'my description', 'www.torontopubliclibrary.ca');
+INSERT INTO assignments (name, teacher_id, subject_id, description, url) VALUES ('assignment6',  1,  2, 'my description', 'www.ontario.ca');
