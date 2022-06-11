f31b34f92489af7b80154928b5792e0cfa45dba6
YuriMacBook <07yuri@gmail.com>
2022-06-11 13:46:46 -0400

    fix calendar label

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -13,15 +13,17 @@ const Calendar = () => {
     <div className="day">Wed 6</div>
     <div className="day">Thu 7</div>
     <div className="day current">Fri 8</div>
-    <div className="day">Sat 9</div>
-    <div className="day">Sun 10</div>
+    <div className="day wknd">Sat 9</div>
+    <div className="day wknd">Sun 10</div>
   </div>
   <div className="content">
     <div className="time" style={{"grid-row": "1"}}></div>
-    <div className="time" style={{"grid-row": "2"}}></div>
+    <div className="time label" style={{"grid-row": "2"}}></div>
     <div className="time" style={{"grid-row": "3"}}></div>
     <div className="time" style={{"grid-row": "4"}}></div>
     <div className="filler-col"></div>
+
+    <div className="col label" style={{"grid-column": "1"}}>Week 3 June 20-26</div>
     <div className="col" style={{"grid-column": "3"}}></div>
     <div className="col" style={{"grid-column": "4"}}></div>
     <div className="col" style={{"grid-column": "5"}}></div>
