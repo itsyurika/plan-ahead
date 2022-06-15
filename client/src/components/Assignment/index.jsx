7d31e34657dda5e0bba649da4c001ef0e33476a2
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-15 13:50:44 -0400

    Add validation to the create form

--- client/src/components/Assignment/index.jsx
+++ client/src/components/Assignment/index.jsx
@@ -6,14 +6,16 @@ const AssignmentView = (props) => {
     <article className='assignment__show modalBackdrop'>
       <div className={`form-modalContainer ${props.status?.toLowerCase()}`}>
         <div id="cancel-X" onClick={props.onBack}>&#10006;</div>
+        {props.status && <div>
         <header id="assignment-header"><h3>{props.title}</h3></header>
-        <p id="assn-subj">{props.subject?.name}</p>
+        <p id="assn-subj">Subject: {props.subject?.name}</p>
         <p id="assn-desc">Description:<br/>{props.description}</p>
         <p id="assn-link">Link: {props.url}</p>
         <p id="assn-prog">Your Progress: {props.status}</p>
+        </div>}
 
-        {props.status && props.status !== 'Not Started' &&< button onClick={props.onCancel}>Cancel</button>}
-        {props.status === 'Not Started' &&< button onClick={props.onStart}>Start</button>}
+        {props.status && props.status !== 'Not Started' && < button onClick={props.onCancel}>Cancel</button>}
+        {props.status === 'Not Started' && < button onClick={props.onStart}>Start</button>}
         {props.status === 'Started' && < button onClick={props.onComplete}>Complete</button>}
         
         {props.admin && <Form {...props} />}
