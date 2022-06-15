b606cc6bd7cd35ceda4507f81fa113fbcbbff9e2
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-15 17:15:26 -0400

    first styling of assignment modals

--- client/src/components/Assignment/index.jsx
+++ client/src/components/Assignment/index.jsx
@@ -1,23 +1,27 @@
 import Form from "./Form";
+import 'components/styles/Assignment.scss';
 
 
 const AssignmentView = (props) => {
+  console.log("looking for status props:", props.status.toLowerCase().trim(" "))
   return (
     <article className='assignment__show modalBackdrop'>
-      <div className={`form-modalContainer ${props.status?.toLowerCase()}`}>
+      <div className={`form-modalContainer ${props.status.toLowerCase().replace(/\s+/g, '')}`}>
+
         <div id="cancel-X" onClick={props.onBack}>&#10006;</div>
         {props.status && <div>
-        <header id="assignment-header"><h3>{props.title}</h3></header>
-        <p id="assn-subj">Subject: {props.subject?.name}</p>
-        <p id="assn-desc">Description:<br/>{props.description}</p>
+        <header ><h1 className={`assignment_header ${props.status.toLowerCase().replace(/\s+/g, '')}`}>{props.title}</h1></header>
+        <p id="assn-subj">{props.subject?.name} </p>
+        <p className={`assn-desc ${props.status.toLowerCase().replace(/\s+/g, '')}`}>Description:</p>
+        <p id="desc">{props.description}</p>
         <p id="assn-link">Link: {props.url}</p>
-        <p id="assn-prog">Your Progress: {props.status}</p>
-
-        {props.status === 'Not started' && < button onClick={props.onStart}>Start</button>}
-        {props.status === 'Started' && < button onClick={props.onComplete}>Complete</button>}
-        {props.status === 'Started' && < button onClick={props.onCancelStarted}>Undo Started</button>}
-        {props.status === 'Complete' && < button onClick={props.onCancelComplete}>Cancel Submission</button>}
-        
+        <p id="assn-prog">Your Progress:&nbsp;&nbsp;<span id="prog-status"> {props.status}! </span></p>
+        <div id="status-buttons">
+        {props.status === 'Not started' && < button className="start-btn" onClick={props.onStart}>Start</button>}
+        {props.status === 'Started' && < button className="start-btn"onClick={props.onComplete}>Complete</button>}
+        {props.status === 'Started' && < button className="undo-btn" onClick={props.onCancelStarted}>Undo Started</button>}
+        {props.status === 'Complete' && < button className="undo-btn" onClick={props.onCancelComplete}>Cancel Submission</button>}
+        </div>
         </div>}
 
 
