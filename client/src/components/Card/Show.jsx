ecbefa3c6e6a9ef5de02341d3744ac3608fd6d76
YuriMacBook <07yuri@gmail.com>
2022-06-16 18:39:14 -0400

    implement border color based on duedate

--- client/src/components/Card/Show.jsx
+++ client/src/components/Card/Show.jsx
@@ -1,7 +1,36 @@
+import { differenceInHours, isPast, parseISO } from "date-fns";
+import classNames from "classnames";
+
 const Show = (props) => {
+  const {dueDate} = props.assigned
+
+  const isPastDue = (dueDate) => {
+    return isPast(parseISO(dueDate)) //returns true if it's pastDue
+  }
+
+  const hrsRemaining = (dueDate) => {
+    return differenceInHours(parseISO(dueDate), new Date())
+  }
+
+  const setBorderColor = (dueDate) => {
+    if((props.status !== 'Complete') && isPastDue(dueDate)) {
+      return 'red'; }
+    if((props.status !== 'Complete') && (hrsRemaining(dueDate) < 12)) {
+      return 'yellow';}
+      return 'default'
+    }  
+  
+    const borderColorClass = classNames('default', {
+      'red': props.status !== 'Complete' && isPastDue(dueDate),
+      'yellow': props.status !== 'Complete' && (hrsRemaining(dueDate) < 12),
+    })
+      
+
+
+ 
   return (
     <main
-      className={`card__show ${props.status?.toLowerCase().replace(/\s+/g, '')}`}
+      className={`card__show ${props.status?.toLowerCase().replace(/\s+/g, '')} ${setBorderColor(dueDate)}` }
       onClick={props.onClick}
     >
       <header><h4>{props.title}</h4></header>
