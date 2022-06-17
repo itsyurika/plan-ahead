7271744ce2b426e801a4d52871693d55f815cada
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-16 20:27:22 -0400

    matches background colours of list assignments to the appropriate status

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -5,25 +5,21 @@ import Assignment from "components/Assignment";
 const List = (props) => {
 
   const viewName = () => {
-    if (props.view === 'pastDue') return 'Past Due'
-    if (props.view === 'complete') return 'Completed'
-    if (props.view === 'art') return 'Art';
-    if (props.view === 'english') return 'English';
-    if (props.view === 'history') return 'History';
-    if (props.view === 'math') return 'Math';
-    if (props.view === 'science') return 'Science';
+    const views = props.view.replace(/([A-Z])/g, ' $1').trim()
+    return views[0].toUpperCase() + views.substring(1)
   };
 
   return (
 
     <section className='list__view'>
       <h1> {viewName()} Assignments </h1>
+      <article className='assignment__show'>
         <ul id='ul-view'>
           {props.assignmentList.map((assignment) => {
       
           return (
-
-          <li className='card-li' key={assignment.id} >< Assignment 
+          
+          <li className={`card-li ${assignment.status.toLowerCase().replace(/\s+/g, '')}`} key={assignment.id} >< Assignment 
             {...assignment }
             view={props.view}
             onStart={() => { props.updateSubmission(assignment.assigned.id, { dateStarted: new Date() }); }}
@@ -33,6 +29,7 @@ const List = (props) => {
           />
           </li>)})}
         </ul>
+        </article>
     </section>
 
   )
