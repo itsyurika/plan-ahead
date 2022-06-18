a86fb9f25964da7163c36487a581e2974863529e
Ben Finlay <benjamin.finlay@gmail.com>
2022-06-18 14:22:58 -0400

    adds particle effects

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,9 +2,10 @@ import 'components/styles/Assignment.scss';
 import Assignment from "components/Assignment";
 
 const Modal = (props) => {
+
   return (
     <article className='outerwrapper'>
-      <div className='modalBackdrop'>
+      <div className={`modalBackdrop ${props.assigned.dateCompleted ? 'complete' : ''}`}>
         <div className='form-modalContainer' >
         <div className='cancel-X' onClick={props.onBack}>&#10006;</div>
           <Assignment  {...props} />
