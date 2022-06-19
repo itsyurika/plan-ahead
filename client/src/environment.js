8424fc4e0244cf5436386ee500161897cc58067d
YuriMacBook <07yuri@gmail.com>
2022-06-19 18:22:58 -0400

    clean up send alert debris

--- client/src/environment.js
+++
@@ -1,9 +0,0 @@
-
-const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
-const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN;
-const messagingServiceSid = process.env.REACT_APP_TWILIO_MSG_SID;
-const phoneNumber = process.env.REACT_APP_PHONENUMBER;
-
-const ENV = {accountSid, authToken, messagingServiceSid, phoneNumber};
-
-module.exports = {ENV};
